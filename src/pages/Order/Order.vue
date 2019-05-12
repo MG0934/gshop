90d35c321cb9d85a14b909eb39d3dd5ac9efac4f
mooq <mugang0934@163.com>
2019-05-12 22:58:08 +0800

    first commit

---
+++ src/pages/Order/Order.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+      Order
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+</style>
