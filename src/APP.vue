62c94b1a06acbac3ec3462556b74bfd1e1455eff
muqing <mugang0934@163.com>
2019-05-14 16:55:48 +0800

    add all pages and commit base js

--- src/APP.vue
+++ src/APP.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <router-view/>
-    <FooterGuide/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,30 +5,53 @@ import Msite from '../pages/Msite/Msite'
 import Order from '../pages/Order/Order'
 import Profile from '../pages/Profile/Profile'
 import Search from '../pages/Search/Search'
+import Login from '../pages/Login/Login'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    routes:[
+    routes: [
         {
-            path:"/msite",
-            component: Msite           
+            path: "/msite",
+            component: Msite,
+            meta: {
+                showFooter: true
+            }
         },
         {
-            path:"/order",
-            component: Order           
+            path: "/order",
+            component: Order,
+            meta: {
+                showFooter: true
+            }
         },
         {
-            path:"/profile",
-            component: Profile           
+            path: "/profile",
+            component: Profile,
+            meta: {
+                showFooter: true
+            }
         },
         {
-            path:"/search",
-            component: Search           
+            path: "/search",
+            component: Search,
+            meta: {
+                showFooter: true
+            }
         },
         {
-            path:"/",
-            redirect: Msite
+            path: "/",
+            component: Msite,
+            meta: {
+                showFooter: true
+            }
+        },
+        {
+            path: "/login",
+            component: Login,
+            meta: {
+                showFooter: false
+            }
         }
     ]
 })
